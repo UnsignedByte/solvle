cbccfe9ef51c2854da735cb371812c3ad3553cf2
UnsignedByte <edlam1889@gmail.com>
2022-02-25 12:47:43 -0800

    update solver

--- src/init.cpp
+++ src/init.cpp
@@ -2,7 +2,7 @@
 * @Author: UnsignedByte
 * @Date:   2022-02-18 21:18:13
 * @Last Modified by:   UnsignedByte
-* @Last Modified time: 2022-02-18 22:25:56
+* @Last Modified time: 2022-02-23 17:51:33
 */
 
 #include <cstdio>
@@ -10,7 +10,7 @@
 
 int main()
 {
-	solver::init("data/compiled.dat", "data/valid.txt", "data/solutions.txt");
+	solver::init("data/compiled.dat", "data/valid.txt", "data/solutions.txt", 1);
 
 
 	return 0;
