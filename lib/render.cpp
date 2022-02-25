4fd2e6b8a8a92cfebcd27776b2688b110646cadb
UnsignedByte <edlam1889@gmail.com>
2022-02-25 12:49:00 -0800

    init empty render

---
+++ lib/render.cpp
@@ -0,0 +1,18 @@
+/*
+* @Author: UnsignedByte
+* @Date:   2022-02-17 08:56:43
+* @Last Modified by:   UnsignedByte
+* @Last Modified time: 2022-02-18 21:26:01
+*/
+
+#include "render.hpp"
+
+namespace render {
+	namespace {
+
+	}
+
+	void init( sf::RenderWindow& window ) {
+		
+	}
+}
---
+++ lib/render.hpp
@@ -0,0 +1,15 @@
+/*
+* @Author: UnsignedByte
+* @Date:   2022-02-17 08:56:36
+* @Last Modified by:   UnsignedByte
+* @Last Modified time: 2022-02-18 21:24:19
+*/
+
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+namespace render
+{
+	void init( sf::RenderWindow& );
+}
